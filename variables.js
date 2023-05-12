c45bf4652984277f23fcf778052920643443c124
carlosjhr12 <carlosjhr12.icloud.com>
2023-05-11 22:44:14 -0300

    subiendo

---
+++ variables.js
@@ -0,0 +1,58 @@
+let nombre = "pepito";
+
+console.log(nombre);
+
+nombre = "juan";
+
+nombre = 22;
+
+console.log(nombre);
+
+const APELLIDO = "perez";
+
+// cgl  hace la funcion de la flecha para arriba
+
+//string -- cadena de textos
+let nombreDeUsuario = "jorge duje";
+
+//number
+let edad = 30;
+
+// numero flotantes
+let precio = 15.5;
+// booleanos ---> true o false
+let esMayorDeEdad = true;
+
+//undefined ---> indefinido
+let numero = undefined;
+
+//null
+let numero2 = null;
+
+// NaN son todas aquellas que no son numeros
+console.log(nombreDeUsuario * edad);
+
+//operadores :
+
+//matematicos
+let n1 = 2;
+let n2 = 5;
+
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+
+console.log(n1 % n2);
+
+let nombre1 = "gaston";
+let apellido1 = "lopez";
+
+// template literals ---> backticks `` alt+96 ``
+
+console.log(`Hola ${nombre1} ${apellido1} como estas? $`);
+
+//logicos
+//comparacion
+// comentario
+// otro
